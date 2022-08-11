52e111f91c123eb146791524fcb8f5da6edaa012
someonecoding <54615836+someonecoding@users.noreply.github.com>
2022-08-12 01:09:32 +0300

    fixed docker and pgsql bug

--- backend/core/config.py
+++ backend/core/config.py
@@ -5,7 +5,7 @@ class Settings:
     DATABASE_NAME = getenv('POSTGRES_DB')
     DATABASE_USER = getenv('POSTGRES_USER')
     DATABASE_PASSWORD = getenv('POSTGRES_PASSWORD')
-    DATABASE_URL = f'postgresql+asyncpg://{DATABASE_USER}:{DATABASE_PASSWORD}@db/{DATABASE_NAME}'
+    DATABASE_URL = f'postgresql+asyncpg://{DATABASE_USER}:{DATABASE_PASSWORD}@database:5432/{DATABASE_NAME}'
 
 
 settings = Settings()
