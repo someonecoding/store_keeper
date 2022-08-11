38231430167ecf70f413ebb008044540ca4bc9c9
someonecoding <54615836+someonecoding@users.noreply.github.com>
2022-08-12 01:12:18 +0300

    changed test endpoint

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from core.config import settings
+
 
 router = APIRouter(prefix='/user', tags=['user'])
 
 
 @router.get('')
 async def test():
-    return [settings.DATABASE_URL]
+    return {"message": "test"}
