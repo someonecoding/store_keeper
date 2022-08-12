14653ea516718fcfc8d009c2d97f186e8f18a53e
someonecoding <54615836+someonecoding@users.noreply.github.com>
2022-08-13 00:44:44 +0300

    Created AuthHandler

---
+++ backend/core/auth/handlers.py
@@ -0,0 +1,13 @@
+from core.config import settings
+from passlib.context import CryptContext
+
+
+class AuthHandler:
+    secret_key = settings.SECRET_KEY
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def get_password_hash(self, password):
+        return self.pwd_context.hash(password)
+
+    def verify_password(self, password, hashed_password):
+        return self.pwd_context.verify(password, hashed_password)
